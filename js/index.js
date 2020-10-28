e09ae5081d874c42583fa781d8643b42761c1797
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2020-10-27 20:10:37 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+const nav = document.querySelector("nav");
+const learnMoreButton = document.querySelector(".top-cover .learn-more");
+const dwnldBtn = document.querySelector(".download");
+
+document.querySelector(".top-cover img").style.bottom = window.scrollY / 4 + "px";
+
+window.addEventListener("scroll", () => {
+  nav.style.background = `rgb(33, 33, 33, ${window.scrollY / 500})`;
+
+  nav.style.fontSize = Math.max(20, 24 -window.scrollY / 100) + "px";
+  document.querySelector(".top-cover img").style.bottom = window.scrollY / 5 + "px";
+
+
+
+  for (let i = 0; i < document.querySelectorAll(".reason").length; i++) {
+    reason = document.querySelectorAll(".reason")[i];
+
+    if (reason.getBoundingClientRect().top < 800 && !reason.classList.contains("shown")) {
+      reason.classList.add("shown");
+    }
+  }
+});
+
+learnMoreButton.addEventListener("click", () => {
+  document.querySelector(".information").scrollIntoView();
+});
+
+dwnldBtn.addEventListener("click", () => {
+  document.documentElement.style.setProperty("--dwnld-clr", "#c321cc");
+  dwnldBtn.textContent = "Downloading...";
+  // window.open("this_will_be_the_download_link");
+  document.querySelector(".dwnld-a").classList.add("show");
+});
