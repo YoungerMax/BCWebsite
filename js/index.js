eb8178d095d09e2c322d0911965cf26105a5ca36
YoungerMax <60242131+YoungerMax@users.noreply.github.com>
2020-10-29 13:37:28 -0400

    Fixes + additions
    - Fixed issue #1
    - Make the images bounce in as well as the text

--- js/index.js
+++ js/index.js
@@ -9,15 +9,24 @@ window.addEventListener("scroll", () => {
   nav.style.background = `rgb(33, 33, 33, ${window.scrollY / 500})`;
 
   nav.style.fontSize = Math.max(20, 24 -window.scrollY / 100) + "px";
-  document.querySelector(".top-cover img").style.bottom = window.scrollY / 5 + "px";
+  document.querySelector(".top-cover img").style.top = "-" + window.scrollY / 3 + "px";
 
 
 
   for (let i = 0; i < document.querySelectorAll(".reason").length; i++) {
     reason = document.querySelectorAll(".reason")[i];
+	
 
     if (reason.getBoundingClientRect().top < 800 && !reason.classList.contains("shown")) {
       reason.classList.add("shown");
+    }
+  }
+  
+  for (let i = 0; i < document.querySelectorAll(".rImg").length; i++) {
+	  img = document.querySelectorAll(".rImg")[i];
+	  
+	  if (img.getBoundingClientRect().top < 900 && !img.classList.contains("shown")) {
+      img.classList.add("shown");
     }
   }
 });
@@ -28,7 +37,7 @@ learnMoreButton.addEventListener("click", () => {
 
 dwnldBtn.addEventListener("click", () => {
   document.documentElement.style.setProperty("--dwnld-clr", "#c321cc");
-  dwnldBtn.textContent = "Downloading...";
+  dwnldBtn.textContent = "Download coming soon!";
   // window.open("this_will_be_the_download_link");
   document.querySelector(".dwnld-a").classList.add("show");
 });
